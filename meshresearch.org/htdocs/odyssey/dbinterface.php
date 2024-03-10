e7569a4954abede6c21be4b165223fbe7f1fdcde
nicholassharples <nicholas.sharples@gmail.com>
2024-03-10 22:54:09 +0000

    Update dbinterface.php
    Added some sliders

--- meshresearch.org/htdocs/odyssey/dbinterface.php
+++ meshresearch.org/htdocs/odyssey/dbinterface.php
@@ -20,11 +20,37 @@ echo "Welcome, you are logged in!";
         Select image to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <br>
-        Number 1: <input type="text" name="number1" id="number1">
+		<br>
+		Planet name: <input type="text" name="planetName" id="planetName">
+		<br>
+		<br>
+		Planet mass : Pluto <input type="range" min="1e16" max="1e21" value="6e18" class="slider" id="massSlider"> Jupiter. Value: <span id="massValue"></span>
         <br>
-        Number 2: <input type="text" name="number2" id="number2">
+		<br>
+		Planet maximum distance : Mercury <input type="range" min="48" max="3700" value="148" class="slider" id="distanceSlider"> Pluto. Value: <span id="distanceValue"></span>
         <br>
+		<br>
         <input type="submit" value="Upload Image" name="submit">
     </form>
+	
+<script>
+var massSlider = document.getElementById("massSlider");
+var massDisplay = document.getElementById("massValue");
+massDisplay.innerHTML = massSlider.value*1e6 + " kg";
+
+massSlider.oninput = function() {
+  massDisplay.innerHTML = this.value*1e6 + " kg";
+}
+
+var distanceSlider = document.getElementById("distanceSlider");
+var distanceDisplay = document.getElementById("distanceValue");
+distanceDisplay.innerHTML = distanceSlider.value*1e6 + " km";
+
+distanceSlider.oninput = function() {
+  distanceDisplay.innerHTML = this.value*1e6 + " km";
+}
+</script>
+
 </body>
 </html>
+
